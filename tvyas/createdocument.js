6c7f26c9b5e48c127e8fc23fcf6d44438ad1b8d9
April King <april@mozilla.com>
2017-03-29 12:01:30 -0500

    update to be under mixed-content-tests.mozqa.com

--- tvyas/createdocument.js
+++ tvyas/createdocument.js
@@ -2,7 +2,7 @@ function createDoc()
   {
   var doc=document.open("text/html","replace");
 
-  doc.write("<!DOCTYPE html><html><body>Do you see an alert?<script src='http://mozilla.github.io/mixed-content-tests/tvyas/script2.js'>\<\/script\>\<\/body>\<\/html>");
+  doc.write("<!DOCTYPE html><html><body>Do you see an alert?<script src='http://mixed-content-tests.mozqa.com/tvyas/script2.js'>\<\/script\>\<\/body>\<\/html>");
 
   doc.close();
   }
--- tvyas/script3.js
+++ tvyas/script3.js
@@ -1,7 +1,7 @@
 //Include HTTP javascript
 var x;
 x=document.createElement("script");
-x.setAttribute("src","http://mozilla.github.io/mixed-content-tests/tvyas/script.js");
+x.setAttribute("src","http://mixed-content-tests.mozqa.com/tvyas/script.js");
 document.body.appendChild(x);
 
 //Include HTTP css
@@ -10,7 +10,7 @@ var y;
 y=document.createElement("link");
 y.setAttribute("rel","stylesheet");
 y.setAttribute("type", "text/css");
-y.setAttribute("href", "http://mozilla.github.io/mixed-content-tests/tvyas/style.css");
+y.setAttribute("href", "http://mixed-content-tests.mozqa.com/tvyas/style.css");
 document.body.appendChild(y);
 */
 
