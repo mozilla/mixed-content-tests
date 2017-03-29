65d2363271ea97eab1e6f58fd7d368b805f88b59
April King <april@mozilla.com>
2017-03-29 12:07:16 -0500

    add more bsterne tests

---
+++ bsterne/tests/62178/static/script/script1.js
@@ -0,0 +1,4 @@
+var r = document.getElementById("result1");
+r.textContent = "LOADED";
+r.parentElement.className = "test fail";
+
---
+++ bsterne/tests/62178/static/script/script2.js
@@ -0,0 +1,4 @@
+var r = document.getElementById("result2");
+r.textContent = "LOADED";
+r.parentElement.className = "test fail";
+
